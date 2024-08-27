5227e66a4949890353566344aa98bc884a5943f9
gabriel0 <Gabriel.orlando@gmail.com>
2024-08-27 19:28:48 -0300

    Cargo puerto cuando no se detecta ninguno por variable

--- api/app/main.py
+++ api/app/main.py
@@ -187,5 +187,5 @@ def run_script():
         return jsonify({'details': e.output, 'error': str(e)})
 
 if __name__ == '__main__':
-    port = int(os.getenv('FINAL_PORT'))  # Lee el puerto desde la variable de entorno
+    port = int(os.getenv('FINAL_PORT',5000))  # Lee el puerto desde la variable de entorno
     app.run(host='0.0.0.0', port=port)
