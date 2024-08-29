4419e636a63b536553c1d1e25112af3eb091e61a
Gabriel <gabriel.orlando@itau.com.ar>
2024-08-29 14:04:56 -0300

    debug false

--- api/app/main.py
+++ api/app/main.py
@@ -189,4 +189,4 @@ def run_script():
 
 if __name__ == '__main__':
     port = int(os.getenv('FINAL_PORT',5000))  # Lee el puerto desde la variable de entorno
-    app.run(host='0.0.0.0', port=port,debug=True)
+    app.run(host='0.0.0.0', port=port,debug=False)
